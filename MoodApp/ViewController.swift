5b22441d931fd0f2dc0d9eeedebe4b3987c48084
Valeriy Popov <complexityclass@gmail.com>
2016-10-04 10:26:22 +0300

    Initial Commit

---
+++ MoodApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoodApp
+//
+//  Created by Valeriy Popov on 04/10/2016.
+//  Copyright © 2016 Valeriy Popov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
